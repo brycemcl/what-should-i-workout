ffdf99c9cb08a9cae64c8e599f6243434d636a2c
Bryce McLachlan <brycemclachlan@gmail.com>
2021-05-13 02:14:08 +0000

    Inital boilerplate

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+import Head from "next/head";
+const page = () => {
+  return (
+    <>
+      <h1>404 - Page Not Found</h1>
+    </>
+  );
+};
+export default page;
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+const page = () => {
+  return (
+    <>
+      <Head>
+        <title>Change me</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="" />
+      </Head>
+    </>
+  );
+};
+export default page;
